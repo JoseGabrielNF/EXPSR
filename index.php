65400f7727458e5e239e213371984000833ce7e5
Vinícius Oliveira <51140104+oliveirabr@users.noreply.github.com>
2020-04-19 20:51:02 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<title>Página inicial - EXPSR</title>
+</head>
+<body>
+	<nav class="menu">
+		<div class="container">
+			<div class="logo">EXPSR</div>
+			<ul>
+				<li><a href="#">Início</a></li>
+				<li><a href="#sobre">Sobre nós</a></li>
+				<li><a href="#">Planos</a></li>
+				<li class="push alt"><a href="#">Entrar</a></li>
+				<li class="alt"><a href="#">Criar conta</a></li>
+			</ul>
+		</div>
+	</nav>
+	<section class="background">
+		<div class="container">
+			<h1>Seja bem-vindo ao <span>EXPSR</span></h1>
+			<p>Armazene e compartilhe as suas melhores memórias!</p>
+		</div>
+	</section>
+	<section>
+		<a name="sobre"></a>
+	</section>
+</body>
+</html>
