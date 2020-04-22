f86d980a703383fb54d05d6ac140144874fa5d40
luizfelps <estudanteluiz@hotmail.com>
2020-04-21 21:32:52 -0400

    Criação da Página de Cadastro

---
+++ cadastro.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="css/style.css">
+	<title>Página inicial - EXPSR</title>
+</head>
+<body >
+<nav id="nav" class="menu">
+		<div class="container">
+			<div class="logo">EXPSR</div>
+			<ul>
+				<li><a href="#">Início</a></li>
+				<li><a href="#sobre">Sobre nós</a></li>
+				<li><a href="#">Planos</a></li>
+				<li class="push alt"><a href="#">Já possui uma conta?</a></li>
+			</ul>
+		</div>
+    </nav>
+    <section class="backgroundCadastro">
+        <div id="cadastro" class="cadastro">
+    	 <form method="post"> 
+    	        <h1>Criar Conta</h1> 
+    	        <p> 
+    	            <h1 class="nomeInput">Usuário:</h1>
+    	            <input required="required" type="text" placeholder="Insira seu nome de usuário" />
+    	        </p>
+    	        <p> 
+    	            <h1 class="nomeInput">E-mail:</h1>
+    	            <input required="required" type="email" placeholder="Insira seu e-mail"/> 
+    	        </p>
+    	        <p>
+    	            <h1 class="nomeInput">Senha:</h1>
+    	            <input required="required" type="password" placeholder="Insira sua senha"/>
+    	        </p>
+    	        <p> 
+    	            <h1 class="nomeInput">Repetir Senha:</h1>
+    	            <input required="required" type="password" placeholder="Repita a senha inserida acima"/>
+    	        </p>
+    	        <p> 
+    	            <input type="submit" value="Criar Conta"/> 
+    	        </p>
+    	        <p class="termo">  
+    	        Ao criar uma conta você concorda com os termos e condições disponiveis
+    	        <a href="#">aqui</a>
+    	        .
+    	        </p>
+    	    </form>   
+        <div>
+	</section>
+	<section>
+		<a name="sobre"></a>
+	</section>
+    
+</body>
+
+</html>
