4954a46590ee0ec738023af2132f773530ef07f7
joseg <josegabrielnf404@gmail.com>
2020-06-28 14:46:42 -0400

    explorer funcionando

--- app/Http/Controllers/ExplorerController.php
+++ app/Http/Controllers/ExplorerController.php
@@ -12,11 +12,16 @@ class ExplorerController extends Controller
     
     public function index() {
 
-        $user = Auth::user();
-        
-        $albums = Album::where('user_id', $user->id)->take(4)->get();
+    $user = Auth::user();
 
-        return view('explorer', ['user' => $user, 'albums' => $albums, 'personal' => true]);
+    $images = Image::select('users.name', 'users.username', 'images.id', 'images.image_path' )
+    ->join('albums', 'albums.id', 'images.album_id')
+    ->join('users', 'users.id', 'albums.user_id')
+    ->where('albums.public',1)->where('album_id','!=', $user->id)->get();
+
+     return view('explorer', [ 'images'=>$images, 'personal' => true]);
     
     }
+   
+  
 }
