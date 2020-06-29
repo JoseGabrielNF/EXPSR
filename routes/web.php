263671374c791b37992ed7f7845a6fd266cc5cd2
Tobias Gehlen <20056854+TobiasGehlen@users.noreply.github.com>
2020-06-28 22:20:45 -0400

    Correções

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home'); 
-Route::get('/explorer', 'ExplorerController@index')->name('explorer');
+Route::get('/explorer', 'ExplorerController@index')->name('explorer')->middleware('auth');
 Route::get('/account/{username}/album/{id}', 'ProfileController@album_perfil')->middleware('auth');
 Route::get('/album/{id}', 'AlbumController@show');
 Route::get('/image/{id}', 'ImageController@show');
